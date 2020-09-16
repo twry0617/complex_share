e587021515445f66f29faa1e55713eaeb03a04ac
twry0617 <twsoccer0617@gmail.com>
2020-09-16 22:39:17 +0900

    [add] routeファイルの追加

--- complexshare/routes/web.php
+++ complexshare/routes/web.php
@@ -20,6 +20,18 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-Route::get('/quit','QuitController@index')->name('quit');
+Route::get('/quit', 'QuitController@index')->name('quit');
 
 Route::post('/quit', 'QuitController@delete')->name('quit.delete');
+
+Route::get('/article', 'ArticleController@index')->name('top');
+
+Route::resource('articles', 'ArticleController', ['only' => ['create', 'store', 'show', 'edit', 'update', 'destroy']]);
+
+Route::resource('comments', 'CommentsController', ['only' => ['store']]);
+
+Route::post('/articles/{articles}/likes', 'LikesController@store');
+
+Route::post('/articles/{articles}/likes/{like}', 'LikesController@destroy');
+
+Route::get('/mypage' , 'MypageController@index');
